fb6c0e316ba6f31889f6f28b034ee7f37fcfd77e
AncaSea <rekasae255@gmail.com>
2022-08-23 21:14:05 +0700

    stash featureA4

--- 3_Basic Version and Branch Management (Git)/Praktikum/MainProgram.dart
+++ 3_Basic Version and Branch Management (Git)/Praktikum/MainProgram.dart
@@ -4,9 +4,8 @@ main() {
    print("Code Development");
    print("============");
    print("fixing bug");
+   print("fixing bug 2");
    print("featureA");
    print("featureA2");
    print("featureA3");
-   print("============");
-   print("featureB");
 }
