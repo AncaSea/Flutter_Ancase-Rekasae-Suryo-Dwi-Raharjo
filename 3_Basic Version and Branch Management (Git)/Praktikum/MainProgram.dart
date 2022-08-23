94d93624745380ee135cc11e931844eb5f4449fc
AncaSea <rekasae255@gmail.com>
2022-08-23 21:45:04 +0700

    add featureB2

--- 3_Basic Version and Branch Management (Git)/Praktikum/MainProgram.dart
+++ 3_Basic Version and Branch Management (Git)/Praktikum/MainProgram.dart
@@ -3,12 +3,8 @@ main() {
    print("============");
    print("Code Development");
    print("============");
-   print("fixing bug");
-   print("featureA");
-   print("featureA2");
-   print("featureA3");
-   print("============");
    print("fixing bug B1");
    print("fixing bug B2");
    print("featureB");
+   print("featureB2");
 }
